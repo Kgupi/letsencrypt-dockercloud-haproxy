a8e67fe07a4f8c8e1570216b595b5fb8679242d8
F <fernando@agavelab.com>
2017-02-02 15:44:06 -0600

    update le scripts

--- letsencrypt/bin/entrypoint.sh
+++ letsencrypt/bin/entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 # Validate required environment variables.
 [[ -z "$DOMAINS" ]] && MISSING="$MISSING DOMAINS"
 [[ -z "$EMAIL" ]] && MISSING="$MISSING EMAIL"
+[[ -z "$MAILGUN_API_KEY" ]] && MISSING="$MISSING MAILGUN_API_KEY"
 if [[ -n "$MISSING" ]]; then
 	echo "Missing required environment variables: $MISSING" >&2
 	exit 1
@@ -12,6 +13,6 @@ fi
 
 # Wait for HAproxy to start before updating certificates on startup.
 # TODO: Use Dockerize, instead of assuming it takes 60 seconds to start.
-(sleep 60; update-certs.sh) &
+(sleep 60; first.sh) &
 
 exec "$@"
